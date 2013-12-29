541bf86088aae182390521bca5bf325dfc6f806f
elgrillo83 <elgrillo83@gmail.com>
2013-12-29 23:03:17 +0100

    generate static_pages controller (which is not tied to any particular Rails resource)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Restauranteur::Application.routes.draw do
+  get "static_pages/index"
   resources :restaurants
 
   # The priority is based upon order of creation: first created -> highest priority.
