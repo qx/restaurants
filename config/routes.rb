ab4c8ede430330e6adb641ff2b104cf07da32274
elgrillo83 <elgrillo83@gmail.com>
2013-12-29 23:04:42 +0100

    set root route to static_pages#index

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Restauranteur::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -42,7 +42,7 @@ Restauranteur::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
