255eacc148b99fdc13746b0c33ad43e841c84564
elgrillo83 <elgrillo83@gmail.com>
2013-12-29 22:59:16 +0100

    add restaurant model specs

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,3 @@
 class Restaurant < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Restaurant do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @restaurant = Restaurant.new(name: 'Momofuku')
+  end
+
+  subject { @restaurant }
+
+  it { should respond_to :name }
+  it { should be_valid }
+
+  describe 'when name is not present' do
+    before { @restaurant.name = ' ' }
+    it { should_not be_valid }
+  end
+
+  describe 'when name is already taken' do
+    before do
+      restaurant_with_same_name = @restaurant.dup
+      restaurant_with_same_name.name = @restaurant.name.upcase
+      restaurant_with_same_name.save
+    end
+
+    it { should_not be_valid }
+  end
+
 end
