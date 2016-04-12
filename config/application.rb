4df6f50d3ad729f1082a12e67a30336d95c1d5dc
oyqx17 <44708869@qq.com>
2016-04-12 11:48:38 +0800

    run

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Restauranteur
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.secret_key_base = 'ae2ff13720f0045818e6090c38f8f3bd803ac2bed0eef944f02903d5c0429adf74c458dcd1aa0c4f567cd7775e53ed217a4e04c5830ffb357e05a63993fdd3ad'
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
